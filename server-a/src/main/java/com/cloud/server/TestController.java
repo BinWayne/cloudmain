2786bae1c31cb199887eafda95e9d7fe0beb70e3
TonyWu <36578067@qq.com>
2022-02-14 21:49:50 +0800

    服务feign超时问题

--- server-a/src/main/java/com/cloud/server/TestController.java
+++ server-a/src/main/java/com/cloud/server/TestController.java
@@ -8,8 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @RequestMapping("/call")
-    public String call(){
+    public String call() throws InterruptedException {
         System.out.println("invoke");
+        Thread.sleep(6000);
         return "hello";
     }
 }
